8c2597d4a070ffd269f921806da966de2e51ddcf
temka <temka14@gmail.com>
2026-09-23 16:29:04 +0500

    CharacterController + 3 здания + большой пол

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+    private CharacterController controller;
+    private float verticalVelocity = 0f;
+    public float gravity = -20f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
@@ -14,7 +22,15 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))    v = 1f;
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))  v = -1f;
 
-        Vector3 move = new Vector3(h, 0, v) * speed * Time.deltaTime;
-        transform.Translate(move);
+        Vector3 move = new Vector3(h, 0, v) * speed;
+
+        if (controller.isGrounded && verticalVelocity < 0f)
+            verticalVelocity = -2f;
+        else
+            verticalVelocity += gravity * Time.deltaTime;
+
+        move.y = verticalVelocity;
+
+        controller.Move(move * Time.deltaTime);
     }
 }
